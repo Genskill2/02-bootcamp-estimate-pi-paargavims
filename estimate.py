a64a36333d648a8c2d98dc632c01a10f16397f1b
paargavims <84776455+paargavims@users.noreply.github.com>
2021-06-01 16:45:31 +0400

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,5 +1,66 @@
 import math
 import unittest
+import random 
+
+  
+
+INTERVAL= 1000 
+
+  
+
+circle_points= 0 
+
+square_points= 0 
+
+
+
+for i in range(INTERVAL**2): 
+
+    rand_x= random.uniform(-1, 1) 
+
+    rand_y= random.uniform(-1, 1) 
+
+  
+
+    
+
+    origin_dist= rand_x**2 + rand_y**2 
+
+  
+
+    
+
+    if origin_dist<= 1: 
+
+        circle_points+= 1 
+
+  
+
+    square_points+= 1 
+
+    pi = 4* circle_points/ square_points 
+
+  
+
+##    print(rand_x, rand_y, circle_points, square_points, "-", pi) 
+
+##    print("\n") 
+
+  
+
+print("Final Estimation of Pi=", pi) 
+
+
+
+def wallis(n):
+ pi = 0.0
+ for i in range(n): 
+  left = (2 * i)/(2 * i - 1)
+  right = (2 * i)/(2 * i + 1) 
+  total = left * right 
+  pi = pi + total 
+ return pi
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
